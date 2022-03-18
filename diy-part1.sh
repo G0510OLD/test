19acc8aa97f2bed92aa3fa5b7ca85f26aee07871
G0510OLD <goldfrom0510@gmail.com>
2022-03-18 17:21:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,6 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# Add a linkease source
+sed -i '$a src-git linkease https://github.com/linkease/istore' feeds.conf.default
